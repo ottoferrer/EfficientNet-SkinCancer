3081e258408ca0c2cac97f41de05f0977ef05a50
otto.ferrer <otto.ferrer@uees.edu.ec>
2025-01-25 22:00:30 -0800

    EfficientnetB4 test keras

--- scripts/test.py
+++ scripts/test.py
@@ -5,9 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import accuracy_score
+from keras.applications import EfficientNetB4
 
 
 print("TensorFlow version:", tf.__version__)
 print("OpenCV version:", cv2.__version__)
 print("Albumentations version:", albumentations.__version__)
 print("NumPy version:", np.__version__)
+model = EfficientNetB4(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+print(model.summary())
